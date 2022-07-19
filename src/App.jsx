d5d9e8a35a7e24443ba427b8fbf423223c61e253
Sahand Masoleh <sahand.masoleh@gmail.com>
2022-07-19 18:31:13 +0200

    [feat] error boundary

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.scss";
 import { useContext } from "react";
+import { Component } from "react";
 import Wizard from "./components/Wizard";
 import Chat from "./components/Chat";
 import { ConnectionContext } from "./contexts/ConnectionContext";
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ConnectionProvider } from "./contexts/ConnectionContext";
 
+class ErrorBoundary extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { hasError: false };
+	}
+
+	static getDerivedStateFromError() {
+		return { hasError: true };
+	}
+
+	render() {
+		if (this.state.hasError) {
+			return <h1>Oh no! Something went wrong...</h1>;
+		}
+		return this.props.children;
+	}
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<ConnectionProvider>
-			<App />
+			<ErrorBoundary>
+				<App />
+			</ErrorBoundary>
 		</ConnectionProvider>
 	</React.StrictMode>
 );
